b93424e7eea6aa7da640236215424d6d45a04f9a
Fabricio Noda <fabricionoda@gmail.com>
2016-03-19 22:49:12 -0300

    ex2

--- src/br/com/db1/java8/certification/assessmenttest/Main.java
+++
@@ -1,8 +0,0 @@
-package br.com.db1.java8.certification.assessmenttest;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/br/com/db1/java8/certification/assessmenttest/ex1.java
@@ -0,0 +1,10 @@
+package br.com.db1.java8.certification.assessmenttest;
+
+public class ex1 {
+    private static int $;
+    public static void main1(String[] args) {
+	    String a_b;
+        System.out.println($);
+        //System.out.println(a_b); // Error:(8, 28) java: variable a_b might not have been initialized
+    }
+}
